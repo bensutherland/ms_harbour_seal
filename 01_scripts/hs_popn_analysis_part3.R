5261b55039a6b790bfd239b93ca91ca41f36d58e
bensutherland <bensutherland7@gmail.com>
2023-07-20 15:19:27 -0700

    Complete rewrite of the final part of the script

--- 01_scripts/hs_popn_analysis_part3.R
+++ 01_scripts/hs_popn_analysis_part3.R
@@ -1,242 +1,156 @@
 # General analysis of harbour seal data (Part 3)
 # 2020-10-13 initialized
-# start by running '01_scripts/hs_popn_analysis_part2.R' 
-#  or loading the previous image
+#  Requires that '01_scripts/hs_popn_analysis_part2.R' was run
 
-# Load previous image
+#### 00. Front Matter ####
+# Clear space
+# rm(list=ls())
+
+# Start by sourcing simple_pop_stats, then load the previous image
 load(file = "03_results/output_coast-sp_pruned_analysis.Rdata")
 
-## Private Alleles, Burrard Inlet
-# Create backup
-#obj_pacific.bck <- obj_pacific 
-#obj_pacific <- obj_pacific.bck
+#### 01. Private alleles, pre-pruning (REMOVED) ####
+# ## Private Alleles, Burrard Inlet
+# # Create backup
+# #obj_pacific.bck <- obj_pacific 
+# #obj_pacific <- obj_pacific.bck
+# 
+# # Explore Burrard Inlet samples further to see if any private alleles are present
+# indNames(obj_pacific)
+# outlier_samples.id <- c("SOG_108", "SOG_131", "SOG_135", "SOG_115", "SOG_127", "SOG_141", "SOG_125", "SOG_136", "SOG_102")
+# nonoutlier_samples.id <- c("SOG_134", "SOG_114", " SOG_123", "SOG_124", "SOG_130", "SOG_117", "SOG_119", "SOG_129", "SOG_101") # haphazardly selected inds from non-Burrard group
+# 
+# # Keep only SOG
+# obj_pacific.sep <- seppop(obj_pacific)
+# obj.SOG <- obj_pacific.sep$SOG
+# 
+# # Create a df defining the strata for each individual
+# strata.df <- as.data.frame(as.character(pop(obj.SOG)), stringsAsFactors = F)
+# colnames(strata.df)[1] <- "indiv.pop"
+# head(strata.df)
+# 
+# # Add column 'repunit'
+# strata.df$repunit <- strata.df$indiv.pop
+# 
+# strata.df[which(indNames(obj.SOG) %in% outlier_samples.id), "repunit"] <- "Burrard"
+# #strata.df[which(indNames(obj.SOG) %in% nonoutlier_samples.id), "repunit"] <- "Burrard" # haphazardly selected, for comparison
+# 
+# # Confirms it works:
+# indNames(obj.SOG)[which(indNames(obj.SOG) %in% outlier_samples.id)]
+# 
+# # Add strat object to genind
+# strata(obj.SOG) <- strata.df
+# obj.SOG
+# table(strata(obj.SOG))
+# 
+# per_repunit.privallele <- private_alleles(obj.SOG, alleles ~ repunit)
+# 
+# #per_repunit.privallele[, 1:5]
+# dim(per_repunit.privallele)
+# 
+# table(per_repunit.privallele["SOG", ])
+# table(per_repunit.privallele["Burrard", ])
+# 
+# which(per_repunit.privallele["Burrard", ]==9)
+# which(per_repunit.privallele["Burrard", ]==6)
+# which(per_repunit.privallele["Burrard", ]==5)
+# 
+# 
+# # # Get raw number of private alleles per locus
+# # pal <- private_alleles(obj.SOG, locus ~ repunit, count.alleles = FALSE)
+# # table(pal) # this only gives a 0 or 1, does not count alleles, allows one to see exact how many private alleles exist
+# # # per repunit
+# # # This shows the number of rows that have a private allele
+# # rowSums(pal)
+# 
+# 
+# #save.image(file = "03_results/output_coast-sp_pruned_analysis.Rdata")
 
-# Explore Burrard Inlet samples further to see if any private alleles are present
-indNames(obj_pacific)
-outlier_samples.id <- c("SOG_108", "SOG_131", "SOG_135", "SOG_115", "SOG_127", "SOG_141", "SOG_125", "SOG_136", "SOG_102")
-nonoutlier_samples.id <- c("SOG_134", "SOG_114", " SOG_123", "SOG_124", "SOG_130", "SOG_117", "SOG_119", "SOG_129", "SOG_101") # haphazardly selected inds from non-Burrard group
-
-# Keep only SOG
-obj_pacific.sep <- seppop(obj_pacific)
-obj.SOG <- obj_pacific.sep$SOG
-
-# Create a df defining the strata for each individual
-strata.df <- as.data.frame(as.character(pop(obj.SOG)), stringsAsFactors = F)
-colnames(strata.df)[1] <- "indiv.pop"
-head(strata.df)
-
-# Add column 'repunit'
-strata.df$repunit <- strata.df$indiv.pop
-
-strata.df[which(indNames(obj.SOG) %in% outlier_samples.id), "repunit"] <- "Burrard"
-#strata.df[which(indNames(obj.SOG) %in% nonoutlier_samples.id), "repunit"] <- "Burrard" # haphazardly selected, for comparison
-
-# Confirms it works:
-indNames(obj.SOG)[which(indNames(obj.SOG) %in% outlier_samples.id)]
-
-# Add strat object to genind
-strata(obj.SOG) <- strata.df
-obj.SOG
-table(strata(obj.SOG))
-
-per_repunit.privallele <- private_alleles(obj.SOG, alleles ~ repunit)
-
-#per_repunit.privallele[, 1:5]
-dim(per_repunit.privallele)
-
-table(per_repunit.privallele["SOG", ])
-table(per_repunit.privallele["Burrard", ])
-
-which(per_repunit.privallele["Burrard", ]==9)
-which(per_repunit.privallele["Burrard", ]==6)
-which(per_repunit.privallele["Burrard", ]==5)
 
 
-# # Get raw number of private alleles per locus
-# pal <- private_alleles(obj.SOG, locus ~ repunit, count.alleles = FALSE)
-# table(pal) # this only gives a 0 or 1, does not count alleles, allows one to see exact how many private alleles exist
-# # per repunit
-# # This shows the number of rows that have a private allele
-# rowSums(pal)
 
 
-#save.image(file = "03_results/output_coast-sp_pruned_analysis.Rdata")
 
-# Move to next script, hs_popn_analysis_part3.R
-
-
-#### 05. Identify top FST markers for several specific BC datasets ####
-##### NBC vs SOG, no Burrard ####
+#### 02. Identify top FST markers ####
+# Calculate FST for SOG vs. NBC and SOG vs. ORE
 obj_pacific_filt.sep <- seppop(x = obj_pacific) 
+
+### SOG vs. NBC
 obj_pacific_filt <- repool(obj_pacific_filt.sep$NBC, obj_pacific_filt.sep$SOG)
 
-# Remove Burrard Inlet outlier individuals
-outlier_samples.id <- c("SOG_108", "SOG_131", "SOG_135", "SOG_115", "SOG_127", "SOG_141", "SOG_125", "SOG_136", "SOG_102")
-keep.inds <- setdiff(indNames(obj_pacific_filt), outlier_samples.id)
-obj_pacific_filt <- obj_pacific_filt[keep.inds]
-
-## Re-calculate AF to remove low MAF variants
-obj.gl <- gi2gl(gi = obj_pacific_filt, parallel = T) # Convert to genlight
-
-# Calculate frequency of second allele
-myFreq <- glMean(obj.gl)
-
-# Ensure each locus second allele is the minor allele
-for(i in 1:length(myFreq)){
-  
-  if(myFreq[i] > 0.5){
-    
-    myFreq[i] <- 1-myFreq[i]
-    
-  }else{
-    
-    myFreq[i] <- myFreq[i]
-    
-  }
-  
-}
-
-## Final MAF filter
-MAF_rem_final <- names(myFreq[which(myFreq < 0.01)])
-length(MAF_rem_final)
-markers_to_keep <- setdiff(x = locNames(obj_pacific_filt), y = MAF_rem_final)
-obj_pacific_filt <- obj_pacific_filt[, loc=markers_to_keep]
-obj_pacific_filt
+## MAF
+maf_filt(data = obj_pacific_filt, maf = 0.01)
+obj_pacific_filt <- obj_maf_filt
+myFreq_filt_SOG_vs_NBC <- myFreq
 
 # Calculate per-locus FST
 per_locus_stats(data = obj_pacific_filt)
+file.copy(from = paste0("03_results/per_locus_stats_", date, ".txt"), to = paste0("03_results/per_locus_stats_SOG_vs_NBC_", date, ".txt"), overwrite = T)
+per_loc_stats_SOG_vs_NBC.df <- per_loc_stats.df
 
-# Save for later
-per_loc_stats_SOG_no_Burrard_v_NBC.df <- per_loc_stats.df
-
-
-
-##### ORE vs SOG, no Burrard ####
+### SOG vs. ORE
 obj_pacific_filt <- repool(obj_pacific_filt.sep$ORE, obj_pacific_filt.sep$SOG)
 
-# Remove Burrard Inlet outlier individuals
-outlier_samples.id <- c("SOG_108", "SOG_131", "SOG_135", "SOG_115", "SOG_127", "SOG_141", "SOG_125", "SOG_136", "SOG_102")
-keep.inds <- setdiff(indNames(obj_pacific_filt), outlier_samples.id)
-obj_pacific_filt <- obj_pacific_filt[keep.inds]
-
-## Re-calculate AF to remove low MAF variants
-obj.gl <- gi2gl(gi = obj_pacific_filt, parallel = T) # Convert to genlight
-
-# Calculate frequency of second allele
-myFreq <- glMean(obj.gl)
-
-# Ensure each locus second allele is the minor allele
-for(i in 1:length(myFreq)){
-  
-  if(myFreq[i] > 0.5){
-    
-    myFreq[i] <- 1-myFreq[i]
-    
-  }else{
-    
-    myFreq[i] <- myFreq[i]
-    
-  }
-  
-}
-
-## Final MAF filter
-MAF_rem_final <- names(myFreq[which(myFreq < 0.01)])
-length(MAF_rem_final)
-markers_to_keep <- setdiff(x = locNames(obj_pacific_filt), y = MAF_rem_final)
-obj_pacific_filt <- obj_pacific_filt[, loc=markers_to_keep]
-obj_pacific_filt
+## MAF 
+maf_filt(data = obj_pacific_filt, maf = 0.01)
+obj_pacific_filt <- obj_maf_filt
+myFreq_filt_SOG_vs_ORE <- myFreq
 
 # Calculate per-locus FST
 per_locus_stats(data = obj_pacific_filt)
+file.copy(from = paste0("03_results/per_locus_stats_", date, ".txt"), to = paste0("03_results/per_locus_stats_SOG_vs_ORE_", date, ".txt"), overwrite = T)
+per_loc_stats_SOG_vs_ORE.df <- per_loc_stats.df
 
-# Save for later
-per_loc_stats_SOG_no_Burrard_v_ORE.df <- per_loc_stats.df
 
-
-##### SOG vs. Burrard ####
-obj_pacific_filt <- obj_pacific_filt.sep$SOG
-
-# Create separate population for Burrard Inlet outlier individuals
-outlier_samples.id <- c("SOG_108", "SOG_131", "SOG_135", "SOG_115", "SOG_127", "SOG_141", "SOG_125", "SOG_136", "SOG_102")
-
-# Obtain the pop for this object
-SOG_only.vec <- pop(obj_pacific_filt)
-SOG_only.vec <- as.character(SOG_only.vec) # convert to character
-SOG_only.vec[which(indNames(obj_pacific_filt) %in% outlier_samples.id)] <- "BUR" # define the outlier samples as 'BUR'
-SOG_only.vec
-
-pop(obj_pacific_filt) <- SOG_only.vec # Re-assign the population with the new BUR and SOG designations
-indNames(obj_pacific_filt)
-
-obj_pacific_filt
-
-## Re-calculate AF to remove low MAF variants
-obj.gl <- gi2gl(gi = obj_pacific_filt, parallel = T) # Convert to genlight
-
-# Calculate frequency of second allele
-myFreq <- glMean(obj.gl)
-
-# Ensure each locus second allele is the minor allele
-for(i in 1:length(myFreq)){
-  
-  if(myFreq[i] > 0.5){
-    
-    myFreq[i] <- 1-myFreq[i]
-    
-  }else{
-    
-    myFreq[i] <- myFreq[i]
-    
-  }
-  
-}
-
-## Final MAF filter
-MAF_rem_final <- names(myFreq[which(myFreq < 0.01)])
-length(MAF_rem_final)
-markers_to_keep <- setdiff(x = locNames(obj_pacific_filt), y = MAF_rem_final)
-obj_pacific_filt <- obj_pacific_filt[, loc=markers_to_keep]
-obj_pacific_filt
-
-# Calculate per-locus FST
-per_locus_stats(data = obj_pacific_filt)
-
-# Save for later
-per_loc_stats_SOG_vs_Burrard.df <- per_loc_stats.df
-
-##### Per region HOBS ##### 
+#### 03. Identify top HOBS markers #### 
+# Calculate per-locus HOBS for BC or US pops separately
 names(obj_pacific_filt.sep)
-# Inspect HOBS for BC inds
+
+### BC
 obj <- repool(obj_pacific_filt.sep$SOG, obj_pacific_filt.sep$NBC)
+maf_filt(data = obj, maf = 0.01)
+obj <- obj_maf_filt
 per_locus_stats(data = obj)
 per_loc_stats_BC.df <- per_loc_stats.df
 
-# Inspect HOBS for US inds
+### US
 obj <- repool(obj_pacific_filt.sep$ORE, obj_pacific_filt.sep$CAL)
+maf_filt(data = obj, maf = 0.01)
+obj <- obj_maf_filt
 per_locus_stats(data = obj)
 per_loc_stats_US.df <- per_loc_stats.df
 
 
-##### Comparing data together ##### 
-head(per_loc_stats_SOG_no_Burrard_v_ORE.df)
-head(per_loc_stats_SOG_vs_Burrard.df)
-head(per_loc_stats_SOG_no_Burrard_v_NBC.df)
+#### 04. Combine population or region-sp data ####
+head(per_loc_stats_SOG_vs_ORE.df)
+head(per_loc_stats_SOG_vs_NBC.df)
 
-SOG_versus_no_burrard.df <- merge(x = per_loc_stats_SOG_no_Burrard_v_NBC.df, per_loc_stats_SOG_no_Burrard_v_ORE.df, by = "mname")
+head(per_loc_stats_BC.df)
+head(per_loc_stats_US.df)
+
+## Rename FST column before merge for downstream plotting
+colnames(per_loc_stats_SOG_vs_ORE.df)[which(colnames(per_loc_stats_SOG_vs_ORE.df)=="Fst")] <- "Fst.SOG.ORE"
+colnames(per_loc_stats_SOG_vs_NBC.df)[which(colnames(per_loc_stats_SOG_vs_NBC.df)=="Fst")] <- "Fst.SOG.NBC"
+
+colnames(per_loc_stats_BC.df)[which(colnames(per_loc_stats_BC.df)=="Hobs")] <- "Hobs.BC"
+colnames(per_loc_stats_US.df)[which(colnames(per_loc_stats_US.df)=="Hobs")] <- "Hobs.US"
+
+## Merge
+SOG_comparisons_FST.df <- merge(x = per_loc_stats_SOG_vs_ORE.df, per_loc_stats_SOG_vs_NBC.df, by = "mname")
 
 BC_and_US_sp_HOBS.df <- merge(x = per_loc_stats_BC.df, y = per_loc_stats_US.df, by = "mname")
 
+## Plot
 pdf(file = "03_results/FST_and_HOBS_region-specific.pdf", width = 9, height = 5)
 par(mfrow=c(1,2))
-plot(x = SOG_versus_no_burrard.df$Fst.x, y = SOG_versus_no_burrard.df$Fst.y
+plot(x = SOG_comparisons_FST.df$Fst.SOG.NBC, y = SOG_comparisons_FST.df$Fst.SOG.ORE
      , xlab = expression(italic(F)[ST] ~ SOG ~ vs. ~ NBC)
      , ylab = expression(italic(F)[ST] ~ SOG ~ vs. ~ ORE)
      , las = 1
 )
-text(x = 0.6, y = 0.5, labels = paste0("n = ", nrow(SOG_versus_no_burrard.df)))
+text(x = 0.6, y = 0.5, labels = paste0("n = ", nrow(SOG_comparisons_FST.df)))
 
-plot(x = BC_and_US_sp_HOBS.df$Hobs.x, y = BC_and_US_sp_HOBS.df$Hobs.y
+plot(x = BC_and_US_sp_HOBS.df$Hobs.BC, y = BC_and_US_sp_HOBS.df$Hobs.US
      , xlab = expression(per ~ locus ~ H[OBS] ~ (SOG ~ and ~ NBC))
      , ylab = expression(per ~ locus ~ H[OBS] ~ (ORE ~ and ~ CAL))
      , las = 1
@@ -255,13 +169,13 @@ table(myFreq.pac < 0.1)
 table(myFreq.atl < 0.1)
 
 length(myFreq.pac)
-length(myFreq.atl)
+length(myFreq.atl) # TODO: double-check this
 
-table(myFreq.pac < 0.1)[2] / length(myFreq.pac) # 57.9%
-table(myFreq.atl < 0.1)[2] / length(myFreq.atl) # 47.9%
+table(myFreq.pac < 0.1)[2] / length(myFreq.pac) # 57.9% # new: 56.3%
+table(myFreq.atl < 0.1)[2] / length(myFreq.atl) # 47.9% # new: 46.5%
 
-table(myFreq.pac < 0.05)[2] / length(myFreq.pac) # 37.0%
-table(myFreq.atl < 0.05)[2] / length(myFreq.atl) # 31.0%
+table(myFreq.pac < 0.05)[2] / length(myFreq.pac) # 37.0% # new: 36.8%
+table(myFreq.atl < 0.05)[2] / length(myFreq.atl) # 31.0% # new: 29.4%
 
 
 # Plot
@@ -304,35 +218,83 @@ write.table(x = myFreq.atl, file = "03_results/allele_freq_retained_loci_atl.txt
 )
 
 
-##### FINAL ALL POPN ANALYSES TO BE ADDED HERE #####
-#### 02. Global PCA, FST, dendrogram ####
+##### 07. Final population analyses ####
+# Go back to original genepop
+obj
 
-# #### Note: will do this after identifying HWE and related outliers ####
-# # note: have not removed HWE outliers yet, ok?  (#TODO#)
-# ## PCA
-# pca_from_genind(data = obj
-#                 , PCs_ret = 4
-#                 , plot_eigen = TRUE
-#                 , plot_allele_loadings = TRUE
-#                 , retain_pca_obj = TRUE
-#                 , colour_file = "00_archive/harbour_seal_pops_colours.csv"
-# )
-# 
-# # Manually run pca_from_genind to pull out the pca1 obj (note: should assign this out to the global enviro as default)
-# pca_scores_result <- pca.obj$scores
-# write.csv(x = pca_scores_result, file = "03_results/pca_scores_result.csv", quote = F, row.names = T)
-# 
-# ## FST
-# calculate_FST(format = "genind", dat = obj, separated = FALSE, bootstrap = TRUE)
-# 
-# ## Dendrogram
-# make_tree(boot_obj = obj, bootstrap = TRUE, nboots = 10000, dist_metric = "edwards.dist", separated = FALSE)
-# 
-# #### /END/ Note: will do this after identifying HWE and related outliers ####
+## Remove related individuals (previously identified)
+length(inds.to.drop)
+keep_inds_all <- setdiff(x = indNames(obj), y = inds.to.drop)
+obj_all <- obj[keep_inds_all]
+obj_all
+
+## MAF 
+maf_filt(data = obj_all, maf = 0.01)
+obj_all <- obj_maf_filt
+
+## HWE
+hwe_eval(data = obj_all, alpha = 0.01)
+
+# Which col contains the pval? 
+head(per_locus_hwe_NFL.df)
+col.oi <- grep(pattern = "Pr", x = colnames(per_locus_hwe_NFL.df))
+
+# Identify hwe outliers
+hwe_outlier_mname_EQB.vec     <-   per_locus_hwe_EQB.df[per_locus_hwe_EQB.df[, col.oi] < 0.01, "mname"]
+hwe_outlier_mname_NFL.vec    <-    per_locus_hwe_NFL.df[per_locus_hwe_NFL.df[, col.oi] < 0.01, "mname"]
+hwe_outlier_mname_LAB.vec    <-    per_locus_hwe_LAB.df[per_locus_hwe_LAB.df[, col.oi] < 0.01, "mname"]
+hwe_outlier_mname_CAL.vec    <-    per_locus_hwe_CAL.df[per_locus_hwe_CAL.df[, col.oi] < 0.01, "mname"]
+hwe_outlier_mname_NBC.vec    <-    per_locus_hwe_NBC.df[per_locus_hwe_NBC.df[, col.oi] < 0.01, "mname"]
+hwe_outlier_mname_ORE.vec    <-    per_locus_hwe_ORE.df[per_locus_hwe_ORE.df[, col.oi] < 0.01, "mname"]
+hwe_outlier_mname_SOG.vec     <-   per_locus_hwe_SOG.df[per_locus_hwe_SOG.df[, col.oi] < 0.01, "mname"]
+
+# Reporting: how many outliers (p < 0.01) per population
+print(length(hwe_outlier_mname_EQB.vec))   # 123   markers out of HWE
+print(length(hwe_outlier_mname_NFL.vec))   # 131   markers out of HWE
+print(length(hwe_outlier_mname_LAB.vec))   #   0   markers out of HWE
+print(length(hwe_outlier_mname_CAL.vec))   # 281   markers out of HWE
+print(length(hwe_outlier_mname_NBC.vec))   #   0   markers out of HWE
+print(length(hwe_outlier_mname_ORE.vec))   # 289   markers out of HWE
+print(length(hwe_outlier_mname_SOG.vec))   # 360   markers out of HWE
+
+# How many total, and how many unique? 
+markers_to_drop_all <- c(hwe_outlier_mname_EQB.vec, hwe_outlier_mname_NFL.vec, hwe_outlier_mname_LAB.vec
+                         , hwe_outlier_mname_CAL.vec, hwe_outlier_mname_NBC.vec, hwe_outlier_mname_ORE.vec, hwe_outlier_mname_SOG.vec
+                         )
+print(paste0("Identified ", length(markers_to_drop_all), " markers out of HWE in at least one population (incl. redundant names)"))
+markers_to_drop_all <- unique(markers_to_drop_all)
+print(paste0("Of these, there are ", length(markers_to_drop_all), " unique markers"))
+
+## Remove HWE outliers
+markers_to_keep_all <- setdiff(x = locNames(obj_all), y = markers_to_drop_all)
+length(markers_to_keep_all) # 9152 markers to keep
+
+obj_all <- obj_all[, loc=markers_to_keep_all]
+obj_all
 
 
+## PCA, all pops
+pca_from_genind(data = obj_all
+                , PCs_ret = 4
+                , plot_eigen = TRUE
+                , plot_allele_loadings = TRUE
+                , retain_pca_obj = TRUE
+                , colour_file = "00_archive/harbour_seal_pops_colours.csv"
+)
 
+# Manually run pca_from_genind to pull out the pca1 obj (note: should assign this out to the global enviro as default)
+pca_scores_result <- pca.obj$scores
+write.csv(x = pca_scores_result, file = "03_results/pca_scores_result_all_pruned.csv", quote = F, row.names = T)
 
+# Rename so the PCA figures are not overwritten
+file.copy(from = "03_results/pca_samples_PC1_v_PC2.pdf", to = "03_results/pca_samples_PC1_v_PC2_all_pruned.pdf", overwrite = TRUE)
+file.copy(from = "03_results/pca_samples_PC3_v_PC4.pdf", to = "03_results/pca_samples_PC3_v_PC4_all_pruned.pdf", overwrite = TRUE)
+
+## FST
+calculate_FST(format = "genind", dat = obj_all, separated = FALSE, bootstrap = TRUE)
+
+## Dendrogram
+make_tree(boot_obj = obj, bootstrap = TRUE, nboots = 10000, dist_metric = "edwards.dist", separated = FALSE)
 
 #### 0.4 Export ####
 # Write out object
